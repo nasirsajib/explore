efd763214329c982a2b76432bf299a6fc1cda2e1
nasirsajib <nasirsajib@gmail.com>
2015-11-02 08:56:10 +0600

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,12 @@
+<?php
+if(!empty($_FILES["fileToUpload"]["name"])){
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $_FILES["fileToUpload"]["name"]))
+{
+echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+}else{
+echo "No File uploaded.";
+}
+}else{
+echo "No File uploaded.";
+}
+?>
